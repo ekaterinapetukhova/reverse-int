9720d2bb6bcce96b771d838170cba1488b83c40e
ekaterinapetukhova <katepthv12@gmail.com>
2023-08-01 15:51:50 +0200

    feat: solution of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const str = n.toString();
+    let arr = str.split("");
+
+    if (arr[0] === "-") {
+        arr.shift();
+    }
+
+    const arrReverse = arr.reverse(),
+        strReverse = arrReverse.join(""),
+        nReverse = Number(strReverse);
+
+    return nReverse;
+};
